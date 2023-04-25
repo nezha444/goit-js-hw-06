a644a7bbd7939f2e54fe8da6625492070e51ed6b
Nikita <bazilik2452@gmail.com>
2023-04-26 01:36:47 +0200

    finish

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,5 +4,3 @@ const output = document.querySelector('#name-output')
 input.addEventListener('input', (event)=>{
     input.value === "" ? output.textContent = "Anonymous" : output.textContent = event.currentTarget.value
 })
-
-//хуйня не пишет анонимус когда ввел текст и вытер его, нужно исправить
